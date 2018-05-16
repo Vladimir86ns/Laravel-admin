65de28dc21d6e7e88ce305d09511930eeb9c2866
vladimir86ns <vladimir86ns@gmail.com>
2018-05-16 21:43:18 +0200

    fixed employee migration

--- app/Employee.php
+++ app/Employee.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'ime',
+        'prezime',
+        'mesto',
+        'godiste',
+        'broj_mobilnog',
+        'broj_kucnog',
+        'pozicija',
+        'permissions',
+        'pocetak_rada'
+    ];
 }
--- database/migrations/2018_05_16_193006_create_employees_table.php
+++ database/migrations/2018_05_16_193006_create_employees_table.php
@@ -19,11 +19,11 @@ class CreateEmployeesTable extends Migration
 			$table->string('prezime')->nullable();
             $table->string('mesto')->nullable();
             $table->dateTime('godiste')->default(date('Y-m-d'));
-            $table->integer('broj mobilnog')->nullable();
-            $table->integer('broj kucnog')->nullable();
+            $table->integer('broj_mobilnog')->nullable();
+            $table->integer('broj_kucnog')->nullable();
             $table->string('pozicija')->nullable();
             $table->text('permissions')->nullable();
-            $table->dateTime('pocetak rada')->default(date('Y-m-d'));
+            $table->dateTime('pocetak_rada')->default(date('Y-m-d'));
         });
     }
 
