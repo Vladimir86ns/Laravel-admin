b4fb6772550db69cda9025f14ae55b7e86bc96bb
vladimir86ns <vladimir86ns@gmail.com>
2018-05-16 21:38:40 +0200

    added employee table and controllers product, employee

--- database/migrations/2018_05_16_184417_create_products_table.php
+++ database/migrations/2018_05_16_184417_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->unsigned();
-            $table->string('name');
+            $table->string('ime');
             $table->string('velicina');
             $table->decimal('cena');
             $table->decimal('trosak');
